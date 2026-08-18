7dc16f5f138097357191832f41b4280bb95749b1
Antonio Ballesteros <antonio@anclora.com>
2026-08-18 06:39:44 +0200

    chore(sec): purge dead turno-app endpoint references, add tests to CI

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,6 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
-  readonly VITE_UNUSED?: string;
-  readonly VITE_TURNO_APP_API_KEY?: string;
-  readonly VITE_TURNO_APP_ENDPOINT_URL?: string;
   readonly VITE_ENABLE_REMOTE_STORAGE?: string;
 }
 
